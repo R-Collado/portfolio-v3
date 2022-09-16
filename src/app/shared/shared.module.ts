239915aba1224477fc50381a911c6a9638301dec
Roger Collado <roger.collado1130@gmail.com>
2022-09-16 20:58:05 +0200

    added about me section

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ArrowdComponent } from './svg/arrowd/arrowd.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ArrowdComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+  ],
+  exports: [
+    ArrowdComponent
   ]
 })
 export class SharedModule { }
---
+++ src/app/shared/svg/arrowd/arrowd.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'svg-arrowd',
+  templateUrl: './arrowd.component.html',
+  styleUrls: ['./arrowd.component.scss']
+})
+export class ArrowdComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
