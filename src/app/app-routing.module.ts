09395c03ccdc01d64b98620cf582824d39ac6082
Roger Collado <roger.collado1130@gmail.com>
2022-09-21 15:42:14 +0200

    added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './core/pages/home/home.component';
+import { ShelfComponent } from './core/pages/shelf/shelf.component';
+import { SkillsComponent } from './core/pages/skills/skills.component';
+import { WorkComponent } from './core/pages/work/work.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent, pathMatch: 'full'}
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'work', component: WorkComponent },
+  { path: 'skills', component: SkillsComponent },
+  { path: 'shelf', component: ShelfComponent },
+  { path: 'resume', component: WorkComponent },
 ];
 
 @NgModule({
--- src/app/core/pages/pages.module.ts
+++ src/app/core/pages/pages.module.ts
@@ -2,12 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { WorkComponent } from './work/work.component';
+import { ShelfComponent } from './shelf/shelf.component';
+import { ResumeComponent } from './resume/resume.component';
+import { SkillsComponent } from './skills/skills.component';
 
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    WorkComponent,
+    ShelfComponent,
+    ResumeComponent,
+    SkillsComponent
   ],
   imports: [
     CommonModule,
